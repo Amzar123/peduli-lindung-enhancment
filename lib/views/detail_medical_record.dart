5ad1ebb77aa5bf1d965f0b26ab693073618ba31d
Amzar123 <ajimuhammadzapar080500@gmail.com>
2022-10-25 11:22:05 +0700

    update name of page

--- lib/views/detail_medical_record.dart
+++ lib/views/detail_medical_record.dart
@@ -12,7 +12,7 @@ class DetailMedicalPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(keluhan),
+        title: Text("Medical Record"),
       ),
     );
   }
